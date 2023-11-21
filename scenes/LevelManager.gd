3a315f105888279275d178a4dcf6b2c78c955ee0
Gerep <daniel.gerep@skiff.com>
2023-11-21 07:21:51 -0300

    46

--- scenes/LevelManager.gd
+++ scenes/LevelManager.gd
@@ -9,7 +9,7 @@ func change_level(levelIndex):
 	if currentLevelIndex >= levelScenes.size():
 		currentLevelIndex = 0
 
-	get_tree().change_scene(levelScenes[currentLevelIndex].resource_path)
+	$"/root/ScreenTransitionManager".transition_to_scene(levelScenes[currentLevelIndex].resource_path)
 
 func increment_level():
 	change_level(currentLevelIndex + 1)
