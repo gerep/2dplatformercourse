27b56e26bc96d4de3d7fc5f3da212f09d5901451
Gerep <daniel.gerep@skiff.com>
2023-11-28 09:19:59 -0300

    63

--- scenes/UI/AnimatedButton.gd
+++ scenes/UI/AnimatedButton.gd
@@ -5,6 +5,7 @@ export(bool) var disableHoverAnimation
 func _ready():
 	connect("mouse_entered", self, "on_mouse_entered")
 	connect("mouse_exited", self, "on_mouse_exited")
+	connect("focus_exited", self, "on_focus_exited")
 	connect("pressed", self, "on_pressed")
 
 func _process(_delta):
@@ -16,9 +17,15 @@ func on_mouse_entered():
 	if !disableHoverAnimation:
 		$HoverAnimationPlayer.play("hover")
 
-func on_mouse_exited():
+func reset_button_state():
 	if !disableHoverAnimation:
 		$HoverAnimationPlayer.play_backwards("hover")
+
+func on_mouse_exited():
+	reset_button_state()
+
+func on_focus_exited():
+	reset_button_state()
 
 func on_pressed():
 	$AudioStreamPlayer.play()
