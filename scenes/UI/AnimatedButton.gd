2e6e380fb444418400b4b9fe7134c0e6c64fa5e8
Gerep <daniel.gerep@skiff.com>
2023-11-28 09:08:28 -0300

    62

--- scenes/UI/AnimatedButton.gd
+++ scenes/UI/AnimatedButton.gd
@@ -1,5 +1,7 @@
 extends Button
 
+export(bool) var disableHoverAnimation
+
 func _ready():
 	connect("mouse_entered", self, "on_mouse_entered")
 	connect("mouse_exited", self, "on_mouse_exited")
@@ -11,10 +13,12 @@ func _process(_delta):
 	rect_pivot_offset = rect_min_size / 2
 
 func on_mouse_entered():
-	$HoverAnimationPlayer.play("hover")
+	if !disableHoverAnimation:
+		$HoverAnimationPlayer.play("hover")
 
 func on_mouse_exited():
-	$HoverAnimationPlayer.play_backwards("hover")
+	if !disableHoverAnimation:
+		$HoverAnimationPlayer.play_backwards("hover")
 
 func on_pressed():
 	$AudioStreamPlayer.play()
--- scenes/UI/OptionsMenu.gd
+++ scenes/UI/OptionsMenu.gd
@@ -4,15 +4,39 @@ signal back_pressed
 
 onready var backButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/BackButton
 onready var windowModeButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/WindowModeButton
+onready var musicRangeControl = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/MusicVolumeContainer/RangeControl
+onready var sfxRangeControl = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/SFXVolumeContainer/RangeControl
 
 var fullscreen = false
 
 func _ready():
 	windowModeButton.connect("pressed", self, "on_window_mode_pressed")
 	backButton.connect("pressed", self, "on_back_pressed")
+	musicRangeControl.connect("percentage_changed", self, "on_music_volume_changed")
+	sfxRangeControl.connect("percentage_changed", self, "on_sfx_volume_changed")
+
+	update_initial_volume_settings()
+	update_display()
 
 func update_display():
 	windowModeButton.text = "WINDOWED" if fullscreen else "FULLSCREEN"
+
+func update_bus_volume(busName, volumePercent):
+	var busIndex = AudioServer.get_bus_index(busName)
+	var volumeDb = linear2db(volumePercent)
+	AudioServer.set_bus_volume_db(busIndex, volumeDb)
+
+func get_bus_volume_percent(busName):
+	var busIndex = AudioServer.get_bus_index(busName)
+	var volumePercent = db2linear(AudioServer.get_bus_volume_db(busIndex))
+	return volumePercent
+
+func update_initial_volume_settings():
+	var musicPercent = get_bus_volume_percent("Music")
+	musicRangeControl.set_current_percentage(musicPercent)
+	
+	var sfxPercent = get_bus_volume_percent("SFX")
+	sfxRangeControl.set_current_percentage(sfxPercent)
 
 func on_window_mode_pressed():
 	fullscreen = !fullscreen
@@ -21,3 +45,11 @@ func on_window_mode_pressed():
 
 func on_back_pressed():
 	emit_signal("back_pressed")
+
+func on_music_volume_changed(percent):
+	print("on_music_volume_changed", percent)
+	update_bus_volume("Music", percent)
+
+func on_sfx_volume_changed(percent):
+	print("on_sfx_volume_changed", percent)
+	update_bus_volume("SFX", percent)
